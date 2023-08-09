2dbbbb7077b3c051a4faefdab987989156e09ec8
yasaidaisuki <peng.dingkai@gmail.com>
2023-08-09 18:46:08 -0400

    change directional lighting, updated room.glb model

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -12,13 +12,18 @@ export default class Environment {
     }
 
     setSunlight() {
-        this.sunLight = new THREE.DirectionalLight("#DFD4C5", 3);
+
+        this.sunLight = new THREE.DirectionalLight("#ffffff", 2);
         this.sunLight.castShadow = true;
         this.sunLight.shadow.camera.far = 20;
         this.sunLight.shadow.mapSize.set(1024,1024);
         this.sunLight.shadow.normalBias = 0.05;
-        this.sunLight.position.set(1.5,7,3);
+        this.sunLight.position.set(-3.5,7,3);
         this.scene.add(this.sunLight);
+
+        // light helper
+        this.lightHelper = new THREE.DirectionalLightHelper(this.sunLight, 3);
+        this.scene.add(this.lightHelper);
     }
 
     resize() {
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -15,7 +15,7 @@ export default class Room {
 
     setModel() {
         this.scene.add(this.actualRoom);
-        this.actualRoom.scale.set(0.11,0.11,0.11);
+        this.actualRoom.scale.set(0.09,0.09,0.09);
         this.actualRoom.rotation.y = Math.PI;
     }
 
