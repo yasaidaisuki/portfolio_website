387efbe44efa3fb1413159b0a5c73f05cdd02ccc
yasaidaisuki <peng.dingkai@gmail.com>
2023-08-22 11:12:00 -0400

    add window light

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -39,7 +39,7 @@ export default class Renderer{
 
     update() {
         this.renderer.setViewport(0, 0, this.sizes.width, this.sizes.height);
-        this.renderer.render(this.scene, this.camera.perspectiveCam );
+        this.renderer.render(this.scene, this.camera.perspectiveCam ); 
        
         // second screen 
         this.renderer.setScissorTest(true);
--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -8,18 +8,18 @@ export default class Environment {
         this.scene = this.experience.scene; 
         this.resources = this.experience.resources;
         
-        this.setSunlight();
+        this.setLight();
     }
 
-    setSunlight() {
+    setLight() {
 
         // sun light aka. directional light
-        this.sunLight = new THREE.DirectionalLight("#ffffff", 0.4);
+        this.sunLight = new THREE.DirectionalLight("#ffffff", 0.45);
         this.sunLight.castShadow = true;
         this.sunLight.shadow.camera.far = 20;
         // resolution
         this.sunLight.shadow.mapSize.set(2048,2048);
-        this.sunLight.shadow.normalBias = 0.05;
+        this.sunLight.shadow.normalBias = 0.01;
         // x y z 
         this.sunLight.position.set(-9,7,3);
         this.scene.add(this.sunLight);
@@ -29,8 +29,32 @@ export default class Environment {
         this.scene.add(this.lightHelper);
 
         // ambient light
-        this.ambientLight = new THREE.AmbientLight("#ffffff", 0.7);
+        this.ambientLight = new THREE.AmbientLight("#E9DBD5", 0.7);
         this.scene.add(this.ambientLight);
+
+        // window light
+
+        this.intensity = 5;
+        this.distance = 3.0;
+        this.angle = Math.PI / 7;
+        this.penumbra = 0.9;
+        this.decay = 1.0;
+        
+        this.windowLight = new THREE.SpotLight( 0xF6DAD0, this.intensity, this.distance, this.angle, this.penumbra, this.decay );
+        this.windowLight.position.set( 1, 1.2, 0.9 );
+
+        this.windowLight.castShadow = true;
+
+        this.windowLight.shadow.mapSize.set(2048,2048);
+        this.windowLight.shadow.bias = 0.00001;
+
+        this.scene.add( this.windowLight );
+  
+
+        // window light helper
+        this.windowLightHelper = new THREE.SpotLightHelper( this.windowLight );
+        this.scene.add(this.windowLightHelper);
+
     }
 
     resize() {
