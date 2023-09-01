17a4438ebb635487e4310fbaa7ed74b934a4346b
yasaidaisuki <peng.dingkai@gmail.com>
2023-09-01 16:23:57 -0400

    redo model

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -13,7 +13,7 @@ export default class Floor {
     setFloor() {
         this.geometry = new THREE.PlaneGeometry(100,100);
         this.material = new THREE.MeshStandardMaterial({
-            color: 0xf8eadd,
+            color: 0xF7F0E1,
         });
         this.plane = new THREE.Mesh(this.geometry, this.material);
         this.scene.add(this.plane);
