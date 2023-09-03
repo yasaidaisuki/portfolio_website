42ae1cc681442d22ea0754b2ad3345851b80afee
yasaidaisuki <peng.dingkai@gmail.com>
2023-09-03 02:20:50 -0400

    added radius animations to sections for left and right

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -238,7 +238,7 @@ export default class Controls {
                 }, "same");
                 this.fourthMoveTimeline.to(this.environment.windowLight, {
                     distance: 2.7,
-                    decay: 2,
+                    decay: 1.5,
                     duration: 0.9,
                     delay: 0.2,
                 }, "same");
@@ -247,12 +247,317 @@ export default class Controls {
             
 
             // Mobile
-            "(max-width: 968px)": function() {
+            "(max-width: 968px)": () => {
+                
+                // Resets
+                this.room.scale.set(0.07, 0.07, 0.07);
+                this.room.position.set(0, 0, 0);
+                this.camera.orthoCam.position.set(0, 6.5, 10);
+
+                // First section
+                this.firstMoveTimeline = new GSAP.timeline({
+                    scrollTrigger: {
+                        trigger: ".first-move",
+                        start: "top top",
+                        end: "bottom bottom",
+                        scrub: 1,
+                        invalidateOnRefresh: true,
+                    }
+                }).to(this.room.scale, {
+                    x: 0.1,
+                    y: 0.1,
+                    z: 0.1,
+                });
+
+                this.firstMoveTimeline.to(this.room.position, {
+                    x: () => {
+                        return this.sizes.width * 0.0012;
+                    }
+                }, "same");
+
+                // window light
+                this.firstMoveTimeline.to(this.environment.windowLight.position, {
+                    x: () => {
+                        return (this.environment.windowLight.position.x + (this.sizes.width * 0.0012));
+                    }
+                }, "same");
+
+                // target
+                this.firstMoveTimeline.to(this.environment.target.position, {
+                    x: () => {
+                        return (this.environment.target.position.x + (this.sizes.width * 0.0012));
+                    }
+                }, "same");
+
+
+                // Second section
+                this.secondMoveTimeline = new GSAP.timeline({
+                    scrollTrigger: {
+                        trigger: ".second-move",
+                        start: "top top",
+                        end: "bottom bottom",
+                        scrub: 1,
+                        invalidateOnRefresh: true,
+                    }
+                });
+
+                // changing room
+                this.secondMoveTimeline.to(this.room.position, {
+                    x: () => {
+                        return 1;
+                    },
+                    z: () => {
+                        return this.sizes.height * 0.001;
+                    },
+                    duration: 0.8,
+                    delay: 0.2,
+                }, "same");
+            
+                this.secondMoveTimeline.to(this.room.scale, {
+                    x: 0.4,
+                    y: 0.4,
+                    z: 0.4,
+                }, "same");
+                
+                // changing light
+                this.secondMoveTimeline.to(this.environment.windowLight.position, {
+                    x: () => {
+                        return (this.experience.world.environment.windowLight.position.x * 3);
+                    },
+                    y: () => {
+                        return (this.experience.world.environment.windowLight.position.y * 3);
+                    },
+                    duration: 0.9,
+                }, "same");
+
+                // lamp light
+                this.secondMoveTimeline.to(this.experience.world.room.lampLight2, {
+                    distance: 4,
+                }, "same");
+                this.secondMoveTimeline.to(this.experience.world.room.lampLight1, {
+                    distance: 4,
+                }, "same");
+
+                // Third section
+                this.thirdMoveTimeline = new GSAP.timeline({
+                    scrollTrigger: {
+                        trigger: ".third-move",
+                        start: "top top",
+                        end: "bottom bottom",
+                        scrub: 1,
+                        invalidateOnRefresh: true,
+                    }
+                });
+                
+                // move room
+                this.thirdMoveTimeline.to(this.room.position, {
+                    x: () => {
+                        return 1.2;
+                    },
+                    z: () => {
+                        return this.sizes.height * 0.004;
+                    },
+                    duration: 0.8,
+                    delay: 0.2,
+                }, "same");
+                this.thirdMoveTimeline.to(this.room.scale, {
+                    x: 0.27,
+                    y: 0.27,
+                    z: 0.27,
+                }, "same");
+
+                // changing light
+                this.thirdMoveTimeline.to(this.environment.windowLight.position, {
+                    x: () => {
+                        return 5.562926;
+                    },
+                    y: () => {
+                        return 3.759329;
+                    },
+                    z: () => {
+                        return 0.333356;
+                    },
+                    duration: 0.9,
+                }, "same");
+
+                this.thirdMoveTimeline.to(this.environment.windowLight, {
+                    distance: 9,
+                    decay: 1.2,
+                    duration: 0.9,
+                }, "same");
+
+                this.thirdMoveTimeline.to(this.environment.windowLight.shadow, {
+                    normalBias: 0.1,
+                }, "same");
+
+                // target
+                this.thirdMoveTimeline.to(this.environment.target.position, {
+                    x: () => {
+                        return 4.7;
+                    },
+                    y: () => {
+                        return 3.5;
+                    },
+                    z: () => {
+                        return 1;
+                    },
+                    duration: 0.9,
+                }, "same");
+
+                // Fourth Movement
+                this.fourthMoveTimeline = new GSAP.timeline({
+                    scrollTrigger: {
+                        trigger: ".fourth-move",
+                        start: "top top",
+                        end: "bottom bottom",
+                        ease: "power1",
+                        scrub: 1,
+                        invalidateOnRefresh: true,
+                    }
+                });
+                this.fourthMoveTimeline.to(this.room.position, {
+                    x: () => {
+                        return this.sizes.width * -0.0015;
+                    },
+                    y: () => {
+                        return 0;
+                    },
+                    z: () => {
+                        return 0;
+                    },
+                    duration: 1,
+                    delay: 0.2,
+                }, "same");
+                this.fourthMoveTimeline.to(this.room.scale, {
+                    x: 0.1,
+                    y: 0.1,
+                    z: 0.1,
+                }, "same");
+
+                // lamp light
+                this.fourthMoveTimeline.to(this.experience.world.room.lampLight2, {
+                    distance: 1,
+                }, "same");
+                this.fourthMoveTimeline.to(this.experience.world.room.lampLight1, {
+                    distance: 1,
+                }, "same");
+
+                // window light
+                this.fourthMoveTimeline.to(this.environment.windowLight.position, {
+                    x: () => {
+                        return ( 1.3 + (this.sizes.width * -0.0015));
+                    },
+                    y: () => {
+                        return 1;
+                    },
+                    z: () => {
+                        return -0.7;
+                    },
+                }, "same");
+
+                // target
+                this.fourthMoveTimeline.to(this.environment.target.position, {
+                    x: () => {
+                        return ( 0.5 + (this.sizes.width * -0.0015));
+                    },
+                    y: () => {
+                        return 0;
+                    },
+                    z: () => {
+                        return 0;
+                    },
+                    duration: 0.9,
+                    delay: 0.2,
+                }, "same");
+                this.fourthMoveTimeline.to(this.environment.windowLight, {
+                    distance: 2.7,
+                    decay: 1.5,
+                    duration: 0.9,
+                    delay: 0.2,
+                }, "same");
+
             },
            
-            "all": function() {
-              
-            },
+            // all
+            "all": () => {
+                this.sections = document.querySelectorAll(".section");
+                this.sections.forEach((section) => {
+
+                    if (section.classList.contains("right")) {
+                        GSAP.to(section, {
+                            borderTopLeftRadius: 10,
+                            scrollTrigger: {
+                                triggers: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            },
+                        });
+                        GSAP.to(section, {
+                            borderBottomLeftRadius: 700,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "bottom bottom",
+                                end: "bottom top",
+                                scrub: 0.6,
+                            },
+                        });
+                    } else {
+                        GSAP.to(section, {
+                            borderTopRightRadius: 10,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            },
+                        });
+                        GSAP.to(section, {
+                            borderBottomRightRadius: 700,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "bottom bottom",
+                                end: "bottom top",
+                                scrub: 0.6,
+                            },
+                        });
+                    }
+                    if (section.classList.contains("left")) {
+                        GSAP.to(section, {
+                            borderTopLeftRadius: 10,
+                            scrollTrigger: {
+                                triggers: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            },
+                        });
+                        GSAP.to(section, {
+                            borderBottomLeftRadius: 10,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "bottom bottom",
+                                end: "bottom top",
+                                scrub: 0.6,
+                            },
+                        });
+                    } else {
+                        GSAP.to(section, {
+                            borderTopLeftRadius: 10,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            },
+                        });
+                    }
+
+                });
+
+                
+                
+            }
               
           }); 
     }
